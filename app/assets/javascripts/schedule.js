e1979ebc8495740cedb80490581519528aea199c
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-21 19:44:14 -0400

    got shift stuffing working!! [#55161430]

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,14 +1,29 @@
-bookies.controller('scheduleController', ['$scope', 'angularFireCollection', function ($scope, angularFireCollection){
-  $scope.items = angularFireCollection(bookies.masterUrl);
-  $scope.id = '9';
-  console.log($scope);''
+bookies.controller('scheduleController', ['$rootScope','$scope', 'firebaseCollection', function ($rootScope,$scope, firebaseCollection){
+  $scope.schedule = firebaseCollection('Schedule');
+  $scope.users = firebaseCollection('Users');
 
-  $scope.shiftStuffing = function(data, selectedUser){
-    if (data){
-      $scope.selectedUser = $scope.user;
+  var shift ={
+      "date": "20130814",
+      "shifts": [
+        {"name": "morning", "waitlist": [], "user_ids": [], "available" : 3},
+        {"name": "mid", "waitlist": [], "user_ids": [], "available" : 3},
+        {"name": "night", "waitlist": [], "user_ids": [], "available" : 3}
+      ]
+    };
 
-      selectedUser.enabled ; 
-    }
-    console.log('shiftStuffing is occuring : ' ,  data);
+  $scope.schedule.add(
+    shift
+    );
+  console.log('uses',$scope.users);
+  console.log('schedule ' , $scope.schedule)
+  console.log('OUTER scope ',$scope);
+  console.log('root',$rootScope);
+  $scope.shiftStuffing = function(day,index,id){
+    console.log('currentShift ', day, index, id);
+    // update my day so it has a user id
+    day.shifts[index].user_ids.push(id);
+    // update collection with this shift?
+    $scope.schedule.update(day);    
   };
+
 }]);
