a80c61216eea83e0c681939877a9fec7c46b6395
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-29 10:08:25 -0400

    added a filter for range

--- app/assets/javascripts/bookies.js
+++ app/assets/javascripts/bookies.js
@@ -4,3 +4,16 @@ bookies.masterUrl = 'https://anicoll-livechat.firebaseio.com';
 bookies.run(['angularFireAuth', function(angularFireAuth){
     angularFireAuth.initialize(bookies.masterUrl,{'name' : 'user', 'path' : '/'});
 }]);
+
+bookies.filter('range',function()
+{
+  return function(input, total)
+  {
+    total = parseInt(total);
+    for(var i=0; i<total; i++)
+    {
+      input.push(i);
+    }
+    return input;
+  }
+})
