52c0e2742dce8a2b18bd312ec8215534f4b990f9
Alyssa Nicoll <mail@alyssanicoll.com>
2013-08-29 10:10:14 -0400

    login -  sign up working

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,36 +1,46 @@
-bookies.controller('loginController', ['$rootScope', '$scope', 'firebaseCollection', 'angularFireAuth', function ($rootScope, $scope, firebaseCollection,angularFireAuth){
+bookies.controller('loginController', ['$rootScope', '$scope', 'firebaseCollection', 'angularFireAuth', '$timeout', function ($rootScope, $scope, firebaseCollection,angularFireAuth, $timeout){
 $scope.loginButtonDisplay = "Log In";
 $scope.userArray = firebaseCollection('Users');
+// $scope.userArray = new Firebase('https://anicoll-livechat.firebaseio.com/Users');
+
+//get log out working
 
   $scope.log_user = function(){
-    angularFireAuth.login('facebook');
+    if (angularFireAuth._authenticated) {
+      angularFireAuth.logout();
+      $scope.loginButtonDisplay = "Log In";
+    } else {
+      angularFireAuth.login('facebook').then(function(){
+      },function(){
+          console.log('There was an error with logging in with facebook.')
+      });
+    }
+  };
+
+  $scope.$on("angularFireAuth:login", function(evt, user) {
     $scope.loginButtonDisplay = "Log Out";
 
-    angularFireAuth.login('facebook').then(function(){
-      // On fb login success then run through db & check 
-      // if user with that fb user id does not exists
-      var user = $scope.userArray.getByKey('id',$rootScope.user.id);
+    // On fb login success then run through db & check 
+    // if user with that fb user id does not exists
+    $timeout(function() {
+      // Figure out where a promise is already att and use it to do .done()
+      //  Result : this block is only called after user exists
+      var user = $scope.userArray.getByKey('id', $rootScope.user.id);
+      console.log(user);
       if(!user){
-        $rootScope.current_user_id = $scope.userArray.add({
-          birthday: $rootScope.user.birthday,
-          email: $rootScope.user.email, 
-          first_name: $rootScope.user.first_name,
-          last_name: $rootScope.user.last_name,
-          user_name: $rootScope.user.displayName,
-          facebook_id: $rootScope.user.id
-        }).name();
+        var user_obj = {};
+        if($rootScope.user.birthday) user_obj.birthday = $rootScope.user.birthday;
+        if($rootScope.user.email) user_obj.email = $rootScope.user.email;
+        if($rootScope.user.first_name) user_obj.first_name = $rootScope.user.first_name;
+        if($rootScope.user.last_name) user_obj.last_name = $rootScope.user.last_name;
+        if($rootScope.user.user_name) user_obj.user_name = $rootScope.user.user_name;
+        if($rootScope.user.id) user_obj.id = $rootScope.user.id;
+      
+        $rootScope.current_user_id = $scope.userArray.add(user_obj).name();
       }else{
         $rootScope.current_user_id = user.$id;
       }
-
-    },function(){
-        console.log('There was an error with logging in with facebook.')
-    }) ;
-
-  };
-
-  $scope.logout = function(){
-    angularFireAuth.logout();
-  };
+    }, 100);
+  });
 
 }]);
