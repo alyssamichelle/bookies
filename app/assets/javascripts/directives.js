52d60375231f7843c52bf5e9e2da0fd6c983ffd3
Alyssa Nicoll <mail@alyssanicoll.com>
2013-11-11 17:42:47 -0500

    Directives.js Modification
    commented out dropdown directive, should delete next

--- app/assets/javascripts/directives.js
+++ app/assets/javascripts/directives.js
@@ -2,21 +2,21 @@ angular.module('bookies').directive('slabText', function() {
   return {
     priority: 99,
     link: function(scope, element, attrs){
-      console.log(arguments);
+      // console.log(arguments);
       $(element[0]).slabText();
     }
   }
 });
 
-angular.module('bookies').directive('dropDown',function(){
-  return {
-    priority: 99,
-    link: function(scope, element, attrs){
-      console.log(arguments);
-      $(element[0]).dropdown({ gutter : 5 });
-    }
-  }
-});
+// angular.module('bookies').directive('dropDown',function(){
+//   return {
+//     priority: 99,
+//     link: function(scope, element, attrs){
+//       // console.log(arguments);
+//       $(element[0]).dropdown({ gutter : 5 });
+//     }
+//   }
+// });
 
 // angular.module('bookies').directive('notification',function(){
 //   return {
