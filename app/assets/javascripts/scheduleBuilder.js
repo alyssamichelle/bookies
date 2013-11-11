8e5f64411238e26a61399fa6ce0a12c7c6c39557
Alyssa Nicoll <mail@alyssanicoll.com>
2013-11-11 17:48:36 -0500

    Modified ScheduleBuilder.js
    added select arrays for HMTL to access, w00t!!!
    took away good defaults for shift names, not sure why I did that...
    Added Andrews god-awful function to "dynamically" build out number of shift-sub-forms needed.
    REDO in angular if you have time.

--- app/assets/javascripts/scheduleBuilder.js
+++ app/assets/javascripts/scheduleBuilder.js
@@ -6,6 +6,68 @@ bookies.controller('scheduleBuilderController', ['$rootScope','$scope', 'angular
     beginningOfMonth = beginningOfMonth.advance({days: 7 - beginningOfMonth.getDay()});
   }
 
+  $scope.meridiemSelects = [
+    {
+      "id": "am",
+      "name": "<img src=\"/assets/utilities/171-sun@2x.png\"/>&nbsp;AM"
+    },
+    {
+      "id": "pm",
+      "name": "<img src=\"/assets/utilities/126-moon@2x.png\"/>&nbsp;PM"
+    }
+  ]
+
+  $scope.timeSelects = {
+    "01:00":{
+      "id": "1:00",
+      "name": "01:00"
+    },
+    "02:00":{
+      "id": "2:00",
+      "name": "02:00"
+    },
+    "03:00":{
+      "id": "3:00",
+      "name": "03:00"
+    },
+    "04:00":{
+      "id": "4:00",
+      "name": "04:00"
+    },
+    "05:00":{
+      "id": "5:00",
+      "name": "05:00"
+    },
+    "06:00":{
+      "id": "6:00",
+      "name": "06:00"
+    },
+    "07:00":{
+      "id": "7:00",
+      "name": "07:00"
+    },
+    "08:00":{
+      "id": "8:00",
+      "name": "08:00"
+    },
+    "09:00":{
+      "id": "9:00",
+      "name": "09:00"
+    },
+    "10:00":{
+      "id": "10:00",
+      "name": "10:00"
+    },
+    "11:00":{
+      "id": "11:00",
+      "name": "11:00"
+    },
+    "12:00":{
+      "id": "12:00",
+      "name": "12:00"
+    }
+  }
+
   $scope.form_month = {};
   $scope.form_month.startOfMonth = beginningOfMonth.toJSON().slice(0, 10);
   $scope.form_month.shiftLeadOpenDate = beginningOfMonth.rewind({days: 4}).toJSON().slice(0, 10);
@@ -13,7 +75,32 @@ bookies.controller('scheduleBuilderController', ['$rootScope','$scope', 'angular
   $scope.form_month.endOfMonth = beginningOfMonth.advance({weeks: 3 ,days: 8}).toJSON().slice(0, 10);
   $scope.form_month.numberOfShiftBlocks = '3';
   $scope.form_month.numberOfPeoplePerShift = '3';
-  $scope.form_month.shiftNames = ["Morning", "Afternoon", "Evening", ""];
+  $scope.form_month.shiftNames = [];
+
+  // on numberOfShiftBlocks change
+  //   create array of empty shift objects
+  //   put them in shiftNames
+  // 
+  // makeShift()
+  // returns object {name:"",startTime:"",endTime:""}
+  //
+  // ng-repeat on shift in shiftNames
+  // ng-model
+
+
+  $scope.onShiftBlockChange = function(count){
+    console.log("onShiftBlockChange")
+    $scope.form_month.shiftNames = [];
+    for (var i = 0; i < count; i++){
+      $scope.form_month.shiftNames.push({
+        name: null,
+        start: null,
+        startMeridiem: null,
+        end: null,
+        endMeridiem: null
+      });
+    }
+  }
 
   $scope.createSchedule = function(){
     $('.create-schedule-button').attr('disabled', true);
@@ -21,40 +108,44 @@ bookies.controller('scheduleBuilderController', ['$rootScope','$scope', 'angular
     $scope.scheduleKeys = [];
     angularFire(keyRef, $scope, 'scheduleKeys').then(function(){
       $scope.scheduleKeys.push($scope.form_month.startOfMonth);
-      // $location.path("/schedule");
-    }).fail(function(){
-      $('.create-schedule-button').removeAttr('disabled');
     });
+
+    // .fail(function(){
+    //   $('.create-schedule-button').removeAttr('disabled');
+    // });
   
+    // console.log('$scope.form_month.startOfMonth',$scope.form_month.startOfMonth);
+    $scope.firebaseSchedule = {};
+    var scheduleRef = new Firebase('https://anicoll-livechat.firebaseio.com/Schedule/'+ $scope.form_month.startOfMonth)
+    angularFire(scheduleRef, $scope, 'firebaseSchedule').then(function(){
+      $scope.firebaseSchedule = $scope.form_month;
+      console.log('$scope.form_month',$scope.form_month);
+      var range = Date.range($scope.firebaseSchedule.startOfMonth, $scope.firebaseSchedule.endOfMonth);
+      $scope.firebaseSchedule.days = {};
 
-    var ref = new Firebase('https://anicoll-livechat.firebaseio.com/Schedule/'+ $scope.form_month.startOfMonth)
-    angularFire(ref, $scope, 'month');
+      for(i = 0; i < $scope.firebaseSchedule.numberOfShiftBlocks; i++ ){};
+      // Looping for every day in the Month
+      range.every('day', function(n){
+        var day ={
+            "date": n.format('{MM}/{dd}/{yyyy}'),
+            "shifts": []
+        };
 
-    $scope.month = $scope.form_month;
-    var range = Date.range($scope.month.startOfMonth, $scope.month.endOfMonth);
-    $scope.month.days = {};
+        // Looping for every shift in the Day
+        for(i = 0; i < $scope.firebaseSchedule.numberOfShiftBlocks; i++ ){
+          var shift = $scope.firebaseSchedule.shiftNames[i];
+          shift['available'] = $scope.firebaseSchedule.numberOfPeoplePerShift;
+          shift['id'] = i;
+          // Give Day the Shift
+          day.shifts.push(shift);
+        };
+        // Give Month.weeks Array day values
+        $scope.firebaseSchedule.days[n.format('{MM}-{dd}')] = day;
 
-    for(i = 0; i < $scope.month.numberOfShiftBlocks; i++ ){};
-    // Looping for every day in the Month
-    range.every('day', function(n){
-      var day ={
-          "date": n.format('{MM}/{dd}/{yyyy}'),
-          "shifts": []
-      };
-
-      // Looping for every shift in the Day
-      for(i = 0; i < $scope.month.numberOfShiftBlocks; i++ ){
-        var shift = {
-          "name": $scope.month.shiftNames[i], 
-          "available": $scope.month.numberOfPeoplePerShift,
-          "id": i
-        }
-        // Give Day the Shift
-        day.shifts.push(shift);
-      };
-      // Give Month.weeks Array day values
-      $scope.month.days[n.format('{MM}-{dd}')] = day;
-    });
+        $location.path("/schedule");
+    })
+  });
+// .fail(function(){alert('Schedule Creation Failed')});
 
     
   $scope.submitThatSchedule = function(){
