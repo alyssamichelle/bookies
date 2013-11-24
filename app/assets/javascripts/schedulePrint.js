f37d04f02cf8a739efbac169c79585f3e7912302
Alyssa Nicoll <mail@alyssanicoll.com>
2013-11-24 02:14:11 -0500

    Fixed Schedule Print Page
    -schedulePrintJS
    	created a month ref with angularFire()
    	dis the month ref
    	instantiated an empty userShiftArray
    	set keys equal to the keys of month.days
    	looped through the length of keys
    	set shift equal to month.days[key].shifts
    	set user_ids (if exist) = to the user_id inside the shift
    
    	created a for loop that loops through all the ids, checks which ones are == to currentUser.id and then STUFFS those matching buddies parent shifts into a userShiftsArray, which the markup is using, w000 go angular

--- app/assets/javascripts/schedulePrint.js
+++ app/assets/javascripts/schedulePrint.js
@@ -1,32 +1,38 @@
 bookies.controller('schedulePrintController', ['$rootScope','$scope', 'angularFire', '$route', 'notify', '$location','$timeout', '$routeParams', function ($rootScope, $scope, angularFire, $route, notify, $location,$timeout, $routeParams){
   $scope.$route = $route;
+  if($routeParams){
+  }else{
+    $routeParams.currentDay = "2014-01-01";
+  }
+  var ref = new Firebase('https://anicoll-livechat.firebaseio.com/Schedule/' + $routeParams.currentDay);
+  $scope.month = {};
+  angularFire(ref, $scope, 'month').then(function(dis){
+    dis();
+    $rootScope.userShiftArray = [];
+    var keys = Object.keys($scope.month.days);
 
-  // gatherPrintInfo($routeParams.currentDay);
+    // for shifts in the month loop
+    for (var i = 0; i < keys.length; i++) {
+      // for users in shifts loop
+      var shifts = $scope.month.days[keys[i]].shifts;
 
-  console.log('days', days);
-  var month = days;
-  $rootScope.userShiftArray = [];
-  var keys = Object.keys(days);
-  console.log('keys',keys);
+      for (var a = 0; a < shifts.length; a++) {
+        if(shifts[a].user_ids){
+          var user_ids = Object.keys(shifts[a].user_ids);
 
-  // for shifts in the month loop
-  for (var i = 0; i < keys.length; i++) {
-    // for users in shifts loop
-    var shifts = month[keys[i]].shifts;
-    console.log('shifts',shifts);
-
-    for (var a = 0; a < shifts.length; a++) {
-      var user_ids = Object.keys(shifts[a].user_ids);
-      console.log('user_ids',user_ids);
-
-      for (var b = 0; b < user_ids.length; b++) {
-        //check if our current user is in them
-        if(user_ids[b] == $rootScope.firebaseUser.id){
-          $rootScope.userShiftArray.push(shifts[a]);
-          console.log($scope.userShiftArray);
-        }
+          for (var b = 0; b < user_ids.length; b++) {
+            //check if our current user is in them
+            console.log('user_ids[b]', user_ids[b],$rootScope.firebaseUser.id);
+            if(user_ids[b] == $rootScope.firebaseUser.id){
+              shifts[a].date = keys[i];
+              console.log('shifts[a]',shifts[a]);
+              $rootScope.userShiftArray.push(shifts[a]);
+              console.log($scope.userShiftArray);
+            }
+          };
+        };
       };
     };
-  };
+  })
 
 }]);
